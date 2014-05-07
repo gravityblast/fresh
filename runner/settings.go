df838178856088f3eb82ae5b3a48828202ea598e
Lateef Jackson <lateef.jackson@gmail.com>
2014-05-06 21:37:35 -0700

    Support -a command line arguments
    For supporting and commandline arugment to pass to the binary
    there is a new '-a' option. Also my editor autoformats to
    gofmt so these files are cleaned up with the standarde format.

--- runner/settings.go
+++ runner/settings.go
@@ -1,138 +1,138 @@
 package runner
 
 import (
-	"fmt"
-	"github.com/pilu/config"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
+  "fmt"
+  "github.com/pilu/config"
+  "os"
+  "path/filepath"
+  "strconv"
+  "strings"
+  "time"
 )
 
 const (
-	envSettingsPrefix   = "RUNNER_"
-	mainSettingsSection = "Settings"
+  envSettingsPrefix   = "RUNNER_"
+  mainSettingsSection = "Settings"
 )
 
 var settings = map[string]string{
-	"config_path":       "./runner.conf",
-	"root":              ".",
-	"tmp_path":          "./tmp",
-	"build_name":        "runner-build",
-	"build_log":         "runner-build-errors.log",
-	"valid_ext":         ".go, .tpl, .tmpl, .html",
-	"build_delay":       "600",
-	"colors":            "1",
-	"log_color_main":    "cyan",
-	"log_color_build":   "yellow",
-	"log_color_runner":  "green",
-	"log_color_watcher": "magenta",
-	"log_color_app":     "",
+  "config_path":       "./runner.conf",
+  "root":              ".",
+  "tmp_path":          "./tmp",
+  "build_name":        "runner-build",
+  "build_log":         "runner-build-errors.log",
+  "valid_ext":         ".go, .tpl, .tmpl, .html",
+  "build_delay":       "600",
+  "colors":            "1",
+  "log_color_main":    "cyan",
+  "log_color_build":   "yellow",
+  "log_color_runner":  "green",
+  "log_color_watcher": "magenta",
+  "log_color_app":     "",
 }
 
 var colors = map[string]string{
-	"reset":          "0",
-	"black":          "30",
-	"red":            "31",
-	"green":          "32",
-	"yellow":         "33",
-	"blue":           "34",
-	"magenta":        "35",
-	"cyan":           "36",
-	"white":          "37",
-	"bold_black":     "30;1",
-	"bold_red":       "31;1",
-	"bold_green":     "32;1",
-	"bold_yellow":    "33;1",
-	"bold_blue":      "34;1",
-	"bold_magenta":   "35;1",
-	"bold_cyan":      "36;1",
-	"bold_white":     "37;1",
-	"bright_black":   "30;2",
-	"bright_red":     "31;2",
-	"bright_green":   "32;2",
-	"bright_yellow":  "33;2",
-	"bright_blue":    "34;2",
-	"bright_magenta": "35;2",
-	"bright_cyan":    "36;2",
-	"bright_white":   "37;2",
+  "reset":          "0",
+  "black":          "30",
+  "red":            "31",
+  "green":          "32",
+  "yellow":         "33",
+  "blue":           "34",
+  "magenta":        "35",
+  "cyan":           "36",
+  "white":          "37",
+  "bold_black":     "30;1",
+  "bold_red":       "31;1",
+  "bold_green":     "32;1",
+  "bold_yellow":    "33;1",
+  "bold_blue":      "34;1",
+  "bold_magenta":   "35;1",
+  "bold_cyan":      "36;1",
+  "bold_white":     "37;1",
+  "bright_black":   "30;2",
+  "bright_red":     "31;2",
+  "bright_green":   "32;2",
+  "bright_yellow":  "33;2",
+  "bright_blue":    "34;2",
+  "bright_magenta": "35;2",
+  "bright_cyan":    "36;2",
+  "bright_white":   "37;2",
 }
 
 func logColor(logName string) string {
-	settingsKey := fmt.Sprintf("log_color_%s", logName)
-	colorName := settings[settingsKey]
+  settingsKey := fmt.Sprintf("log_color_%s", logName)
+  colorName := settings[settingsKey]
 
-	return colors[colorName]
+  return colors[colorName]
 }
 
 func loadEnvSettings() {
-	for key, _ := range settings {
-		envKey := fmt.Sprintf("%s%s", envSettingsPrefix, strings.ToUpper(key))
-		if value := os.Getenv(envKey); value != "" {
-			settings[key] = value
-		}
-	}
+  for key, _ := range settings {
+    envKey := fmt.Sprintf("%s%s", envSettingsPrefix, strings.ToUpper(key))
+    if value := os.Getenv(envKey); value != "" {
+      settings[key] = value
+    }
+  }
 }
 
 func loadRunnerConfigSettings() {
-	if _, err := os.Stat(configPath()); err != nil {
-		return
-	}
+  if _, err := os.Stat(configPath()); err != nil {
+    return
+  }
 
-	logger.Printf("Loading settings from %s", configPath())
-	sections, err := config.ParseFile(configPath(), mainSettingsSection)
-	if err != nil {
-		return
-	}
+  logger.Printf("Loading settings from %s", configPath())
+  sections, err := config.ParseFile(configPath(), mainSettingsSection)
+  if err != nil {
+    return
+  }
 
-	for key, value := range sections[mainSettingsSection] {
-		settings[key] = value
-	}
+  for key, value := range sections[mainSettingsSection] {
+    settings[key] = value
+  }
 }
 
 func initSettings() {
-	loadEnvSettings()
-	loadRunnerConfigSettings()
+  loadEnvSettings()
+  loadRunnerConfigSettings()
 }
 
 func getenv(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
+  if value := os.Getenv(key); value != "" {
+    return value
+  }
 
-	return defaultValue
+  return defaultValue
 }
 
 func root() string {
-	return settings["root"]
+  return settings["root"]
 }
 
 func tmpPath() string {
-	return settings["tmp_path"]
+  return settings["tmp_path"]
 }
 
 func buildName() string {
-	return settings["build_name"]
+  return settings["build_name"]
 }
 func buildPath() string {
-	return filepath.Join(tmpPath(), buildName())
+  return filepath.Join(tmpPath(), buildName())
 }
 
 func buildErrorsFileName() string {
-	return settings["build_log"]
+  return settings["build_log"]
 }
 
 func buildErrorsFilePath() string {
-	return filepath.Join(tmpPath(), buildErrorsFileName())
+  return filepath.Join(tmpPath(), buildErrorsFileName())
 }
 
 func configPath() string {
-	return settings["config_path"]
+  return settings["config_path"]
 }
 
 func buildDelay() time.Duration {
-	value, _ := strconv.Atoi(settings["build_delay"])
+  value, _ := strconv.Atoi(settings["build_delay"])
 
-	return time.Duration(value)
+  return time.Duration(value)
 }
