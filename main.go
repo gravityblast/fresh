2d65002b593899a225f18e239ce6991c677ff69e
Lateef Jackson <lateef.jackson@gmail.com>
2014-05-06 21:37:35 -0700

    Support -a command line arguments
    For supporting and commandline arugment to pass to the binary
    there is a new '-a' option. Also my editor autoformats to
    gofmt so these files are cleaned up with the standarde format.

--- main.go
+++ main.go
@@ -13,24 +13,25 @@ Traffic (https://github.com/pilu/traffic) already has a middleware that shows th
 package main
 
 import (
-  "flag"
-  "fmt"
-  "github.com/lateefj/fresh/runner"
-  "os"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pilu/fresh/runner"
 )
 
 func main() {
-  configPath := flag.String("c", "", "config file path")
-  flag.Parse()
+	configPath := flag.String("c", "", "config file path")
+	flag.Parse()
 
-  if *configPath != "" {
-    if _, err := os.Stat(*configPath); err != nil {
-      fmt.Printf("Can't find config file `%s`\n", *configPath)
-      os.Exit(1)
-    } else {
-      os.Setenv("RUNNER_CONFIG_PATH", *configPath)
-    }
-  }
+	if *configPath != "" {
+		if _, err := os.Stat(*configPath); err != nil {
+			fmt.Printf("Can't find config file `%s`\n", *configPath)
+			os.Exit(1)
+		} else {
+			os.Setenv("RUNNER_CONFIG_PATH", *configPath)
+		}
+	}
 
-  runner.Start()
+	runner.Start()
 }
