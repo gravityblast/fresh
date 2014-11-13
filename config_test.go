9462eea47d2aabe90e456f0eccfca0452bebc131
Andrea Franz <andrea@gravityblast.com>
2014-11-13 19:01:29 +0000

    more test on configuration

--- config_test.go
+++ config_test.go
@@ -36,6 +36,7 @@ func TestParseConfig(t *testing.T) {
 
   [section_3]
   `
+
 	reader := bufio.NewReader(strings.NewReader(content))
 	sections, err := parseConfig(reader, "main")
 
@@ -43,34 +44,54 @@ func TestParseConfig(t *testing.T) {
 	assert.Equal(t, 4, len(sections))
 
 	// Main section
-	// mainSection := sections[0]
-	// assert.Equal(t, 2, len(mainSection.Commands))
-	// assert.Equal(t, "1", mainSection["foo"])
-	// assert.Equal(t, "2", mainSection["bar"])
+	mainSection := sections[0]
+	assert.Equal(t, 2, len(mainSection.Commands))
+	tests := [][]string{
+		{"foo", "1"},
+		{"bar", "2"},
+	}
 
-	// // Section 1
-	// section1 := sections["section_1"]
-	// assert.Equal(t, 5, len(section1))
-	// assert.Equal(t, "3", section1["foo"])
-	// assert.Equal(t, "4", section1["bar"])
-	// assert.Equal(t, "5 6", section1["baz"])
-	// assert.Equal(t, "7", section1["qux"])
-	// assert.Equal(t, "", section1["quux"])
+	for i, opts := range tests {
+		assert.Equal(t, opts[0], mainSection.Commands[i].Name)
+		assert.Equal(t, opts[1], mainSection.Commands[i].CmdString)
+	}
 
-	// // Section 2
-	// section2 := sections["section_2"]
-	// assert.Equal(t, 9, len(section2))
-	// assert.Equal(t, "1", section2["a"])
-	// assert.Equal(t, "2", section2["b"])
-	// assert.Equal(t, "3", section2["c"])
-	// assert.Equal(t, "4", section2["d"])
-	// assert.Equal(t, "5", section2["e"])
-	// assert.Equal(t, "6", section2["f"])
-	// assert.Equal(t, "7", section2["g"])
-	// assert.Equal(t, "8", section2["h"])
-	// assert.Equal(t, "http://example.com", section2["url"])
+	// Section 1
+	section1 := sections[1]
+	assert.Equal(t, "section_1", section1.Name)
+	tests = [][]string{
+		{"foo", "3"},
+		{"bar", "4"},
+	}
 
-	// // Section 3
-	// section3 := sections["section_3"]
-	// assert.Equal(t, 0, len(section3))
+	for i, opts := range tests {
+		assert.Equal(t, opts[0], section1.Commands[i].Name)
+		assert.Equal(t, opts[1], section1.Commands[i].CmdString)
+	}
+
+	// Section 2
+	section2 := sections[2]
+	assert.Equal(t, "section_2", section2.Name)
+	assert.Equal(t, 9, len(section2.Commands))
+	tests = [][]string{
+		{"a", "1"},
+		{"b", "2"},
+		{"c", "3"},
+		{"d", "4"},
+		{"e", "5"},
+		{"f", "6"},
+		{"g", "7"},
+		{"h", "8"},
+		{"url", "http://example.com"},
+	}
+
+	for i, opts := range tests {
+		assert.Equal(t, opts[0], section2.Commands[i].Name)
+		assert.Equal(t, opts[1], section2.Commands[i].CmdString)
+	}
+
+	// Section 3
+	section3 := sections[3]
+	assert.Equal(t, "section_3", section3.Name)
+	assert.Equal(t, 0, len(section3.Commands))
 }
