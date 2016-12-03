a088a546d50893a6750ace0bff0f4676c52ce8ee
Gregory McCue <gsmccue@gmail.com>
2016-12-03 19:45:07 +0100

    Refactor utils_test.

--- runner/utils_test.go
+++ runner/utils_test.go
@@ -2,27 +2,45 @@ package runner
 
 import (
 	"testing"
-
-	assert "github.com/pilu/miniassert"
 )
 
 func TestIsWatchedFile(t *testing.T) {
-	// valid extensions
-	assert.True(t, isWatchedFile("test.go"))
-	assert.True(t, isWatchedFile("test.tpl"))
-	assert.True(t, isWatchedFile("test.tmpl"))
-	assert.True(t, isWatchedFile("test.html"))
+	tests := []struct {
+		file     string
+		expected bool
+	}{
+		{"test.go", true},
+		{"test.tpl", true},
+		{"test.tmpl", true},
+		{"test.html", true},
+		{"test.css", false},
+		{"test-executable", false},
+		{"./tmp/test.go", false},
+	}
 
-	/* // invalid extensions */
-	assert.False(t, isWatchedFile("test.css"))
-	assert.False(t, isWatchedFile("test-executable"))
+	for _, test := range tests {
+		actual := isWatchedFile(test.file)
 
-	// files in tmp
-	assert.False(t, isWatchedFile("./tmp/test.go"))
+		if actual != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, actual)
+		}
+	}
 }
 
 func TestIsIgnoredFolder(t *testing.T) {
-	assert.True(t, isIgnoredFolder("assets/node_module"))
-	assert.False(t, isIgnoredFolder("app/controllers"))
-	assert.True(t, isIgnoredFolder("tmp/pid"))
+	tests := []struct {
+		dir      string
+		expected bool
+	}{
+		{"assets/node_modules", true},
+		{"tmp/pid", true},
+		{"app/controllers", false},
+	}
+
+	for _, test := range tests {
+		actual := isIgnoredFolder(test.dir)
+		if actual != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, actual)
+		}
+	}
 }
